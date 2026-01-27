3f2794d999571a1a890021066afd95438c12dc21
Afeefa Batool <99643705+Afeefa-Batool@users.noreply.github.com>
2026-01-27 14:30:34 +0500

    remove old model configuration

--- app.py
+++ app.py
@@ -67,8 +67,7 @@ if uploaded_file:
 
             # Call LLaMA Vision Model
             response = client.chat.completions.create(
-# model="llama-3.2-90b-vision-preview",
-               model = "meta-llama/llama-4-scout-17b-16e-instruct",
+            model = "meta-llama/llama-4-scout-17b-16e-instruct",
                 messages=[
                     {
                         "role": "user",
